cf3cd79176f59f840a898df9cf480dc7119e0c8a
Gaganugraha92 <gaganugraha33@gmail.com>
2018-11-21 11:52:29 +0700

    add instrumen testing
    add instrumen testing last match, next macth, favorite list

---
+++ app/src/androidTest/java/nugraha/angga/com/footballmatch/main/MainActivityTest.kt
@@ -0,0 +1,89 @@
+package nugraha.angga.com.footballmatch.main
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.runner.AndroidJUnit4
+import nugraha.angga.com.footballmatch.view.MainActivity
+import org.junit.Rule
+import android.support.test.rule.ActivityTestRule
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import nugraha.angga.com.footballmatch.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule
+    @JvmField var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testLastMatchBehaviour() {
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_navigation))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.prev_match)).perform(ViewActions.click())
+
+        Thread.sleep(2500)
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, ViewActions.click()))
+    }
+
+    @Test
+    fun testNextMatchBehaviour() {
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_navigation))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.next_match)).perform(ViewActions.click())
+
+        Thread.sleep(2500)
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, ViewActions.click()))
+    }
+
+    @Test
+    fun testNextFavoriteBehaviour() {
+
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_navigation))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.next_match)).perform(ViewActions.click())
+
+        Thread.sleep(2500)
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, ViewActions.click()))
+
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite)).perform(ViewActions.click())
+
+        Espresso.pressBack()
+
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_navigation))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.favorites)).perform(ViewActions.click())
+
+        Thread.sleep(2500)
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvMatch)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+    }
+
+}
